f81d2afa27ebd50c30bfcb36567688c7cefe5a34
bkilty <brennan.kilty@gmail.com>
2015-12-04 16:46:07 -0700

    added echo feature and a port status, but the port status still needs work.

--- BlinkFromScratchonXLP16.X/main.c
+++ BlinkFromScratchonXLP16.X/main.c
@@ -45,11 +45,14 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
  */
 
 #include "mcc_generated_files/mcc.h"
-#include "stdlib.h"
-#include "math.h"
-#include "string.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-
+/* Function Prototypes*/
+uint8_t uartSend(const uint8_t* message, uint8_t messageLength);
+void uartPrintStatus();
 
 /*
                          Main application
@@ -58,17 +61,22 @@ int main(void) {
     // initialize the device
     SYSTEM_Initialize();
     uint16_t portValue;
-    char introduction[]="Here are some messages from PIC. \r\n";
-    char append[4][3];
-    uint8_t appendCount = 0;
-    uint8_t uartBuffer[128];
+    char introduction[]="\r\nHere are some messages from PIC. \r\n";
+
     uint8_t i = 0;
-    char currentCountAscii[16];
+    uint8_t j = 0;
     uint8_t sLength = 0;
+
+    //UART Data Variables
+    uint8_t uartBytes2Rx;
+    uint8_t uartRxBuffer[128];
+    uint8_t uartBytes2Tx;
+    uint8_t uartTxBuffer[128];
+    UART2_TRANSFER_STATUS uartTxStatus ;
+    UART2_STATUS uartStatus;
     uint8_t uartBytesWritten = 0;
-    uint8_t* tempPtr;
-    uint16_t tmrValue;
-    //UART2_TRANSFER_STATUS status ;
+    
+    
     
     //intialize the Board Power VDD_Board
     IO_RB2_SetLow(); 
@@ -103,60 +111,155 @@ int main(void) {
      */
 
       
-    memcpy((void*)append[0], (void*)"th",3);
-    memcpy((void*)append[1], (void*)"st",3);
-    memcpy((void*)append[2], (void*)"nd",3);
-    memcpy((void*)append[3], (void*)"rd",3);
-    
+        
     
     while (1) {
         
         //IO_RB15_SetLow();
         
-        // Add your application code
         // Read RB7
-        //portValue = IO_RB7_GetValue();
+        portValue = IO_RB7_GetValue();
         //if(portValue == 1)
+            //print the status of the UART2 Peripheral
+            //uartPrintStatus();
         
         if(i == 0)
         {
-            strcpy(uartBuffer, introduction);
-            sLength = strlen(uartBuffer);
+            // char * strcpy(char *, const char *);
+            strcpy((char*)uartTxBuffer, (const char *)introduction);
+            //size_t strlen(const char *);
+            sLength = (uint8_t)strlen((char*)uartTxBuffer);
+           // uartBytes2Tx = sLength;
+            uartSend(uartTxBuffer,sLength);
             i++;
-            
-        }else{      
-         appendCount = i%10;
-         if(appendCount > 3)
-             appendCount = 0;
-         sLength = sprintf(uartBuffer, "PIC %d%s Transmission\r\n", i,append[appendCount]);
-         i++;
+               
         }
-        uartBytesWritten = 0;
         
-        while(uartBytesWritten < sLength)
+        
+        if(i!=0)
         {
-            //status = UART2_TransferStatusGet ( ) ;
-            if (!UART2_TransmitBufferIsFull())
-            {
-                tempPtr = uartBuffer + uartBytesWritten;
-                uartBytesWritten += UART2_WriteBuffer(uartBuffer+uartBytesWritten, UART2_TransmitBufferSizeGet());
-            }               
-            UART2_TasksTransmit();
-             
-            
+            //TMR3_delay_ms(_XTAL_FREQ, 3000);//wait for 10ms in sleep mode
+            uartTxStatus = UART2_TransferStatusGet();
+            uartStatus = UART2_StatusGet();
+            if(UART2_TRANSFER_STATUS_RX_DATA_PRESENT & uartTxStatus)
+            {                              
+                //Get bytes from the UART Receiver
+                uartBytes2Rx = UART2_ReadBuffer(uartRxBuffer, 128); //get the bytes
+                
+                //echo back what what received from Raspberry PI
+                uartSend(uartRxBuffer, uartBytes2Rx);
+                //uartBytes2Tx = sprintf((char*) uartTxBuffer, "\r\n PIC Received %d bytes --> %s\r\r\r\n", uartBytes2Rx, uartRxBuffer);
+                //uartSend(uartTxBuffer, uartBytes2Tx);
+                
+                //clean up the TxBuffer and RxBuffer void * memset(void *, int, size_t);
+                memset((void*)uartRxBuffer, NULL, uartBytes2Rx);
+                memset((void*)uartTxBuffer, NULL, uartBytes2Tx);
+                 
+            }
+          
         }
-        if(i==1)
-            TMR3_delay_ms(_XTAL_FREQ, 3000); //delay just so I can see the first message
-        //else
-          //  TMR3_delay_ms(_XTAL_FREQ, 10);
         
+        
+        //if(i==1)
+        //    TMR3_delay_ms(_XTAL_FREQ, 3000); //delay just so I can see the first message
+        
+    
     }
-
+        
     return -1;
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+/*uartPrintStatus -- get status of UART Channel 2 and send it over UART2
+ * 
+ * UART2_STATUS enumeration
+        //   UART2_RX_DATA_AVAILABLE = (1 << 0) 
+        //   UART2_RX_OVERRUN_ERROR = (1 << 1) 
+        //   UART2_FRAMING_ERROR = (1 << 2) 
+        //   UART2_PARITY_ERROR = (1 << 3) 
+        //   UART2_RECEIVER_IDLE = (1 << 4) 
+        //   UART2_TX_COMPLETE = (1 << 8)
+        //   UART2_TX_FULL = (1 << 9) 
+
+     
+        UART2_TRANSFER_STATUS 
+        //UART2_TRANSFER_STATUS_RX_FULL = (1 << 0) 
+        //UART2_TRANSFER_STATUS_RX_DATA_PRESENT = (1 << 1)
+        //UART2_TRANSFER_STATUS_RX_EMPTY = (1 << 2)
+        //UART2_TRANSFER_STATUS_TX_FULL = (1 << 3) 
+        //UART2_TRANSFER_STATUS_TX_EMPTY = (1 << 4) 
+*///////////////////////////////////////////////////////////////////////////////
+
+void uartPrintStatus()
+{
+    UART2_TRANSFER_STATUS uartTferStatus ;
+    UART2_STATUS uartStatus;
+    char statusMessage[256];
+    uint8_t tempStat; //
+    uint8_t sLength = 0;
+    uint8_t uartBytes2Rx = 0; //how many bytes are in the receiver
+    
+    //Get peripheral status
+    uartTferStatus = UART2_TransferStatusGet();
+    uartStatus = UART2_StatusGet();
+    
+    uartBytes2Rx = UART2_ReceiveBufferSizeGet(); //how many bytes are in the receiver
+   
+    //Put status into an ascii string
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\r\n RxBytes Avail --> %d",uartBytes2Rx );
+    tempStat = uartTferStatus & UART2_TRANSFER_STATUS_RX_FULL;
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\r\n UART2_TxFER_STATUS_RX_FULL --> %d",tempStat );
+    tempStat = uartStatus & UART2_TRANSFER_STATUS_RX_DATA_PRESENT;
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\r\n RX_DATA_PRESENT --> %d",tempStat );
+    tempStat = uartStatus & UART2_RX_OVERRUN_ERROR;
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\r\n UART2_RX_OVERRUN_ERROR ----> %d",tempStat );
+    tempStat = uartStatus & UART2_FRAMING_ERROR;
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\nUART2_FRAMING_ERROR -> %d",tempStat);
+    tempStat = uartStatus & UART2_PARITY_ERROR;
+    sLength += (uint8_t)sprintf((char*)(statusMessage+sLength), (const char*)"\r\nUART2_PARITY_ERROR -----------> %d",tempStat);
         
+    uartSend((const uint8_t*)statusMessage, sLength);
+    
+    return 0;
+        
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// uartSend -- send bytes over UART channel 2
+//////////////////////////////////////////////////////////////////////////////
+uint8_t uartSend(const uint8_t* message, uint8_t messageLength)
+{
+    uint8_t uartBytesWritten = 0;
+    UART2_TRANSFER_STATUS uartTStatus ;
+    UART2_STATUS uartStatus;
+    
+            
+        while( uartBytesWritten < messageLength)
+        {
+            uartTStatus = UART2_TransferStatusGet() ;
+            if (uartTStatus & UART2_TRANSFER_STATUS_TX_EMPTY)
+            {
+                uartBytesWritten += UART2_WriteBuffer ( message + uartBytesWritten, messageLength - uartBytesWritten )  ;
+                if(uartBytesWritten < messageLength)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                continue;
+            }
+
+            // Do something else...
+        }
+    return uartBytesWritten;
+}
 /**
  End of File
  */
